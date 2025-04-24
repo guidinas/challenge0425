c7990fec087b246dd65749fce011b378f95f80e1
guilhermina.dias <guilhermina.dias@ifood.com.br>
2025-04-24 15:00:29 -0300

    Basic folder structure

---
+++ app/src/main/java/io/reflectoring/api/Main.java
@@ -0,0 +1,11 @@
+package io.reflectoring.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
