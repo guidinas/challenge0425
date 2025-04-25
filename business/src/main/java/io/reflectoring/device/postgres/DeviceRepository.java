ef8d43fa8d2ebdb74581495be681c9c18aa556ef
guilhermina.dias <guilhermina.dias@ifood.com.br>
2025-04-25 20:15:27 -0300

    JDBC configuration

---
+++ business/src/main/java/io/reflectoring/device/postgres/DeviceRepository.java
@@ -0,0 +1,9 @@
+package io.reflectoring.device.postgres;
+
+import io.device.models.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+}
---
+++ vendors/postgres/src/main/java/io/reflectoring/postgres/JdbcConfiguration.java
@@ -0,0 +1,12 @@
+package io.reflectoring.postgres;
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@Configuration
+public class JdbcConfiguration {
+
+
+}
