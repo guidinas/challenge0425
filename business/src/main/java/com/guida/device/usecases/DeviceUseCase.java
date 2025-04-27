97521217539ba69939376f60d200b031019de1a7
guilhermina.dias <guilhermina.dias@ifood.com.br>
2025-04-27 00:37:29 -0300

    Controller completed with requests

--- business/src/main/java/com/guida/device/usecases/DeviceUseCase.java
+++ business/src/main/java/com/guida/device/usecases/DeviceUseCase.java
@@ -2,26 +2,33 @@ package com.guida.device.usecases;
 
 import com.guida.converters.CreateDeviceRequestToDevice;
 import com.guida.converters.DeviceToDeviceRequestResponse;
+import com.guida.device.validators.DeviceUpdateValidator;
 import com.guida.models.CreateDeviceRequest;
 import com.guida.models.Device;
 import com.guida.device.postgres.DeviceRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+import java.util.List;
+import java.util.UUID;
+
+@Component
 public class DeviceUseCase {
     public DeviceUseCase(
             DeviceRepository deviceRepository,
             CreateDeviceRequestToDevice createDeviceRequestToDevice,
-            DeviceToDeviceRequestResponse deviceToDeviceRequestResponse) {
+            DeviceToDeviceRequestResponse deviceToDeviceRequestResponse,
+            List<DeviceUpdateValidator> deviceUpdateValidators) {
         this.deviceRepository = deviceRepository;
         this.createDeviceRequestToDevice = createDeviceRequestToDevice;
         this.deviceToDeviceRequestResponse = deviceToDeviceRequestResponse;
+        this.deviceUpdateValidators = deviceUpdateValidators;
     }
 
     final DeviceRepository deviceRepository;
     final CreateDeviceRequestToDevice createDeviceRequestToDevice;
     final DeviceToDeviceRequestResponse deviceToDeviceRequestResponse;
+    final List<DeviceUpdateValidator> deviceUpdateValidators;
 
     public ResponseEntity<Object> addDevice(CreateDeviceRequest createDeviceRequest){
         final Device deviceToCreate = createDeviceRequestToDevice.toDevice(createDeviceRequest);
@@ -40,11 +47,63 @@ public class DeviceUseCase {
         final Device deviceFound = deviceRepository.findById(deviceToCreate.getId());
 
         if(deviceFound == null){
-            return ResponseEntity.ok(deviceRepository.save(deviceToCreate));
-        } else {
             return  ResponseEntity.badRequest()
-                    .body("Device already exists");
+                    .body("Device does not exists");
+        } else {
+            try {
+                deviceUpdateValidators.forEach(validator -> validator.validate(deviceToCreate, deviceFound));
+            }catch (IllegalArgumentException e){
+                return ResponseEntity.badRequest()
+                        .body(e.getMessage());
+            }
+           return ResponseEntity.ok(deviceRepository.save(deviceToCreate));
         }
     }
 
+    public ResponseEntity<Object> getDevice(String uuid) {
+        try{
+            final UUID id = UUID.fromString(uuid);
+            final Device deviceFound = deviceRepository.findById(id);
+            if(deviceFound == null){
+                return ResponseEntity.badRequest()
+                        .body("Device does not exists");
+            }else {
+                return ResponseEntity.ok(deviceFound);
+            }
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest()
+                    .body("Invalid UUID");
+        }
+
+    }
+
+    public ResponseEntity<Object> getAllDevices() {
+            final List<Device> allDevices = deviceRepository.findAll();
+            if(allDevices.isEmpty()){
+                return ResponseEntity.badRequest()
+                        .body("No devices found");
+            }else {
+                return ResponseEntity.ok(allDevices);
+            }
+    }
+
+    public ResponseEntity<Object> getDevicesByBrand(String brand) {
+        final List<Device> allDevices = deviceRepository.findAll();
+        if(allDevices.isEmpty()){
+            return ResponseEntity.badRequest()
+                    .body("No devices found");
+        }else {
+            return ResponseEntity.ok(allDevices.stream().filter(device -> brand.equalsIgnoreCase(device.getBrand())));
+        }
+    }
+
+    public ResponseEntity<Object> getDevicesByState(String state) {
+        final List<Device> allDevices = deviceRepository.findAll();
+        if(allDevices.isEmpty()){
+            return ResponseEntity.badRequest()
+                    .body("No devices found");
+        }else {
+            return ResponseEntity.ok(allDevices.stream().filter(device -> state.equalsIgnoreCase(device.getState().toString())));
+        }
+    }
 }
--- entrypoint/api/src/main/java/com/guida/DeviceManagerController.java
+++ entrypoint/api/src/main/java/com/guida/DeviceManagerController.java
@@ -1,15 +1,10 @@
 package com.guida;
 
 import com.guida.models.CreateDeviceRequest;
-import com.guida.models.DeviceRequestResponse;
 import com.guida.device.usecases.DeviceUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
@@ -17,18 +12,42 @@ public class DeviceManagerController {
     @Autowired
     DeviceUseCase deviceUseCase;
 
-@PostMapping("/create")
+    @PostMapping("/create")
     public ResponseEntity<Object> addDevice(
             @RequestBody CreateDeviceRequest createDeviceRequest
     ){
-    return deviceUseCase.addDevice(createDeviceRequest);
-}
+        return deviceUseCase.addDevice(createDeviceRequest);
+    }
 
     @PostMapping("/update")
     public ResponseEntity<Object> updateDevice(
             @RequestBody CreateDeviceRequest createDeviceRequest
     ){
         return deviceUseCase.updateDevice(createDeviceRequest);
+    }
+
+    @GetMapping("/get/{uuid}")
+    public ResponseEntity<Object> getDevice(@PathVariable String uuid){
+        return deviceUseCase.getDevice(uuid);
+    }
+
+    @GetMapping("/get-all-devices")
+    public ResponseEntity<Object> getAllDevices(){
+        return deviceUseCase.getAllDevices();
+    }
+
+    @GetMapping("/get/brand")
+    public ResponseEntity<Object> getDevicesByBrand(
+            @RequestBody CreateDeviceRequest createDeviceRequest
+    ){
+        return deviceUseCase.getDevicesByBrand(createDeviceRequest.getBrand());
+    }
+
+    @GetMapping("/get/state")
+    public ResponseEntity<Object> getDevicesByState(
+            @RequestBody CreateDeviceRequest createDeviceRequest
+    ){
+        return deviceUseCase.getDevicesByBrand(createDeviceRequest.getState().toString());
     }
 
 }
